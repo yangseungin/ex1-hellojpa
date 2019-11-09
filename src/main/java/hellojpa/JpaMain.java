0f7a0d1912397d44cb5a6a519e6fb5a735945818
seungin yang <rhfpdk92@naver.com>
2019-11-09 17:59:47 +0900

    영속성 컨텍스트

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,30 +17,32 @@ public class JpaMain {
 
         try {
 
-//            등록
-//            Member member = new Member();
-//            member.setId(3L);//long값이라 L붙임
-//            member.setName("helloC");
+//            쿼리가 한번만 실행됨
+            Member findmember1 = em.find(Member.class, 101L);//sql조회
+            Member findmember2 = em.find(Member.class, 101L);//1차캐시조회
+
+//            영속 엔티티의 동일성 보장
+            System.out.println("result = " + (findmember2==findmember2));
+
+//            쓰기지연
+//            Member memberA=new Member(150L,"A");
+//            Member memberB=new Member(160L,"B");
+//
+//
+//            em.persist(memberA);
+//            em.persist(memberB);
+
+//            엔티티 수정 - 변경감지
+                Member member = em.find(Member.class,160L);
+                member.setName("zzzzzz");
+
+
+
+
+            System.out.println("=========="); // 선 출력이후 쿼리가 나
+
+//                변경이후 새로 저장하는 코드를 쓰지않는다
 //            em.persist(member);
-
-//            찾기
-//            Member findMember = em.find(Member.class, 1L);
-//            System.out.println("findMember = " + findMember.getId());
-//            System.out.println("findMember = " + findMember.getName());
-
-//            수정
-//            Member findMember = em.find(Member.class, 1L);
-//            findMember.setName("HelloJPA"); //커밋하는 시점에 변화가있는지 jpa가 확인하기때문에 따로 작업을 하지 않는다
-
-//            JPQL
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(8)
-                    .getResultList();
-
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
-            }
 
 
             tx.commit();
