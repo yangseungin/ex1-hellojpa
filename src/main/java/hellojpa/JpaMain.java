3645b351a9e632301696753071bc23fa573b3068
seungin yang <rhfpdk92@naver.com>
2019-12-18 00:34:36 +0900

    프록시와 연관관계 관리- 즉시로딩과 지연로딩

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -3,6 +3,7 @@ package hellojpa;
 import org.hibernate.Hibernate;
 
 import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -14,27 +15,28 @@ public class JpaMain {
         tx.begin();
 
         try {
+
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            em.flush();
+            em.clear();
             Member member1 = new Member();
             member1.setUsername("member1");
+            member1.setTeam(team);
             em.persist(member1);
 
 
             em.flush();
             em.clear();
 
-            Member refMember = em.getReference(Member.class, member1.getId());
-            System.out.println("refMember = " + refMember.getClass()); //Proxy
-//            refMember.getUsername(); //강제 초기화
-//            System.out.println("isLoaded= "+ emf.getPersistenceUnitUtil().isLoaded(refMember));
-
-            Hibernate.initialize(refMember); // 강제 초기화  - JPA표준에는 강제초기화가 없음
-
-
-            //1. 이미 영속성 컨텍스트에 1차캐시로 있어서 member를 내뱉음
-            //2. JPA에서는 ==비교가 한 영속성 컨텍스트안에서 가져온놈은 true를 반환해줘야함
-//            Member reference = em.find(Member.class, member1.getId());
-//            System.out.println("reference = " + reference.getClass());
-//            System.out.println("a == a: "+(refMember ==reference));
+//            Member m = em.find(Member.class, member1.getId());
+            List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
+            //즉시로딩은 jpql에서 N+1문제를 일으킴.
+            //전부 lazy로 사용하고 필요시에 fetch join로 가져올수있음
+            //manytoone, onetoone은 기본이 즉시로딩으로 되어있음(다 LAZY로 적어줘야함)
+            //onetoMany는 기본이 lazy
 
 
             tx.commit();
